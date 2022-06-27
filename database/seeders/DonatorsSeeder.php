3a7f04171723d9bb23e7037fb6a26dfb499736b3
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2022-06-27 21:30:33 +0700

    Update seeder

--- database/seeders/DonatorsSeeder.php
+++ database/seeders/DonatorsSeeder.php
@@ -30,44 +30,5 @@ class DonatorsSeeder extends Seeder
             'address_donators' => 'Jl.Kediri Raya Weeh, Jawa Timur',
             'point_donators' => mt_rand(0, 100)
         ]);
-
-        Donators::create([
-            'id_donators' => Uuid::uuid4()->toString() . "\n",
-            'name_donators' => 'Alif Yanuar Aditya Subagyo',
-            'email_donators' => 'michelyanuar@gmail.com',
-            'password_donators' => Hash::make(12345),
-            'gender_donators' => 'male',
-            'blood_type_donators' => 'A',
-            'rhesus_type_donators' => 'negative',
-            'contact_donators' => '087772934259',
-            'address_donators' => 'Jln. Isekai, mantep mantep aselole jos jos jos',
-            'point_donators' => mt_rand(0, 100)
-        ]);
-
-        Donators::create([
-            'id_donators' => Uuid::uuid4()->toString() . "\n",
-            'name_donators' => 'Bagus Tri Atmaja',
-            'email_donators' => 'bagustri734@gmail.com',
-            'password_donators' => Hash::make(12345),
-            'gender_donators' => 'male',
-            'blood_type_donators' => 'A',
-            'rhesus_type_donators' => 'negative',
-            'contact_donators' => '082145198847',
-            'address_donators' => 'Jl bukan jokowi no 69 kyoto, jepang',
-            'point_donators' => mt_rand(0, 100)
-        ]);
-
-        Donators::create([
-            'id_donators' => Uuid::uuid4()->toString() . "\n",
-            'name_donators' => 'Muhammad Ihsan Izzulhaq',
-            'email_donators' => 'mi.izzulhaq@gmail.com',
-            'password_donators' => Hash::make(12345),
-            'gender_donators' => 'male',
-            'blood_type_donators' => 'B',
-            'rhesus_type_donators' => 'negative',
-            'contact_donators' => '081286226512',
-            'address_donators' => 'Jl. Aster, Kab. Tokyo - Japan',
-            'point_donators' => mt_rand(0, 100)
-        ]);
     }
 }
