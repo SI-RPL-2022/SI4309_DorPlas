d87b2792411410906b62c32a4cb29b67b18d7859
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2022-06-28 02:06:24 +0700

    [Browser Testing] Add logout function in LoginTest

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -52,4 +52,28 @@ class LoginTest extends DuskTestCase
                 ->assertSee('Email atau password tidak cocok');
         });
     }
+
+    /** @test */
+    public function user_can_logout()
+    {
+        $user = collect([
+            'email_donators' => 'muhammad.rezki.ananda@gmail.com',
+            'password_donators' => '12345'
+        ]);
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/login')
+                ->assertSee('Masuk')
+                ->type('email_donators', $user['email_donators'])
+                ->type('password_donators', $user['password_donators'])
+                ->press('Masuk')
+                ->assertPathIs('/dashboard')
+                ->ClickLink('Keluar')
+                ->whenAvailable('#logoutModal', function ($modal) {
+                    $modal->pause(1000)
+                        ->assertSee('Anda yakin ingin logout ?')
+                        ->press('Keluar');
+                });
+        });
+    }
 }
